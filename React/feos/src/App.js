1afdadaba5394a78aaf4ed8e8581109550dfd162
Jucagovi <jc.gomez@edu.gva.es>
2022-01-11 18:52:39 +0100

    cambio

--- React/feos/src/App.js
+++ React/feos/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const loco = "Feo";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload { loco }.
         </p>
         <a
           className="App-link"
