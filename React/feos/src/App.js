2c735ad0abae92b07cadebe54ccb63b05de26e9f
Jucagovi <jc.gomez@edu.gva.es>
2022-01-12 17:28:07 +0100

    Instituto

--- React/feos/src/App.js
+++ React/feos/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 
 function App() {
   const loco = "Cambio desde Ryzen 7";
+  const otro = "Otro cambio desde el instituto";
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload {loco}.
+          Edit <code>src/App.js</code> and save to reload {loco} - {otro}.
         </p>
         <a
           className="App-link"
