9f384a299c047db0def4faf56ff0d3e7bdcb700f
Jucagovi <jc.gomez@edu.gva.es>
2022-01-17 18:58:10 +0100

    Clase

--- React/feos/src/App.js
+++ React/feos/src/App.js
@@ -3,16 +3,20 @@ import PrimerComponente from "./Componentes/PrimerComponente";
 import Peliculas from "./Componentes/Peliculas";
 import ConsumirAPI from "./Componentes/ConsumirAPI";
 import Cargando from "./Componentes/Cargando";
+import Reloj from "./Componentes/Reloj";
+import Contador from "./Componentes/Contador";
 
 function App() {
   return (
     <div className="App">
+      <Contador />
       <PrimerComponente nombre="Feo">
         <p>Desde el childrens del componente</p>
         {/* Lo contenido en la etiqueta es props.children */}
       </PrimerComponente>
       <PrimerComponente nombre="Horrible" />
       <Cargando texto="Cargando datos..." />
+      <Reloj />
       <ConsumirAPI />
     </div>
   );
