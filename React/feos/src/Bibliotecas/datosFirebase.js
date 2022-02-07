16cbf3ede64b760f6a54d7e6215168f0af699d4a
Jucagovi <jc.gomez@edu.gva.es>
2022-02-07 12:02:05 +0100

    des

--- React/feos/src/Bibliotecas/datosFirebase.js
+++ React/feos/src/Bibliotecas/datosFirebase.js
@@ -2,21 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  /*
   apiKey: "AIzaSyDhdQ0PGgIItJkWu8LaEtUtbLP6JZNzClQ",
   authDomain: "feos-90bb3.firebaseapp.com",
   projectId: "feos-90bb3",
   storageBucket: "feos-90bb3.appspot.com",
   messagingSenderId: "243531445588",
   appId: "1:243531445588:web:94e7412fb00744e6b504c1",
+*/
 
-  /*
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  */
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
