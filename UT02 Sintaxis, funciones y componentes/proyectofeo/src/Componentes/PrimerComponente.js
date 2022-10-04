a75796de94692b041755b44524cbfcd774bce68a
Jucagovi <jc.gomez@edu.gva.es>
2022-10-04 11:44:58 +0200

    Primer Componente

--- UT02 Sintaxis, funciones y componentes/proyectofeo/src/Componentes/PrimerComponente.js
+++ UT02 Sintaxis, funciones y componentes/proyectofeo/src/Componentes/PrimerComponente.js
@@ -2,10 +2,11 @@ import React from "react";
 
 // const PrimerComponente = (props) => {
 function PrimerComponente(props) {
+  const texto = "Esto es un compoenente de prueba";
   return (
     <React.Fragment>
-      <h1>Hola {props.nombre} desde mi primer componente</h1>
-      <h2>{props.children}</h2>
+      <h1>Hola mundo desde mi primer componente</h1>
+      <h2>{texto}</h2>
     </React.Fragment>
   );
 }
